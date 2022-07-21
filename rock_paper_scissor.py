96c002ff62668035f2190ad4909399e65710ef95
Ramakm <itsramakrushna@gmail.com>
2022-07-21 06:59:54 +0000

    Commit changes in if else

--- rock_paper_scissor.py
+++ rock_paper_scissor.py
@@ -1,6 +1,5 @@
 import random
 
-
 rock = '''
     _______
 ---'   ____)
@@ -28,24 +27,25 @@ scissors = '''
 ---.__(___)
 '''
 
-user_choice = int(input("What do you choose? Type 0 for Rock, 1 for Paper or 2 for Scissors."))
-                     
-# input_choice = [rock, paper, scissors]
+game_images = [rock, paper, scissors]
 
-computer_random_choice = random.randint(0,2)
+user_choice = int(input("What do you choose? Type 0 for Rock, 1 for Paper or 2 for Scissors.\n"))
+print(game_images[user_choice])
 
-print(f"Computer Choice is {computer_random_choice}")
+computer_choice = random.randint(0, 2)
+print("Computer chose:")
+print(game_images[computer_choice])
 
-if user_choice ==0 and computer_random_choice==2:
-  print("You win")
-  
-elif user_choice<computer_random_choice:
-  print("You loose")
-
-elif computer_random_choice == user_choice:
-  print('Draw')
-
-else:
-  print("You typed a invalid number, You loose")
-
+if user_choice >= 3 or user_choice < 0: 
+  print("You typed an invalid number, you lose!") 
+elif user_choice == 0 and computer_choice == 2:
+  print("You win!")
+elif computer_choice == 0 and user_choice == 2:
+  print("You lose")
+elif computer_choice > user_choice:
+  print("You lose")
+elif user_choice > computer_choice:
+  print("You win!")
+elif computer_choice == user_choice:
+  print("It's a draw")
 
