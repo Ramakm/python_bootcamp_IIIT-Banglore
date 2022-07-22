b0cb37d820c6ec118782db0d346cb083b3431bac
Ramakm <itsramakrushna@gmail.com>
2022-07-22 05:23:49 +0000

    Commit newly added files

--- README.md
+++ README.md
@@ -6,3 +6,8 @@ Its a python bootcamp repo, following the #100DayOfCode Challange by Dr. Angela 
           problems are available here. check it out.
           
           1. Rock-Paper-Scissor problem readme. <a href="https://replit.com/@ramakrushna8594/rock-paper-scissors-end#README.md">QDescription</a> 
+        2. FizzBuzz: 
+
+        3. Life in Weeks:
+
+        4. 
---
+++ life_in_ weeks.py
@@ -0,0 +1,19 @@
+#Instructions:
+#I was reading this article by Tim Urban - Your Life in Weeks and realised just how little time we actually have.
+#https://waitbutwhy.com/2014/05/life-weeks.html
+#Create a program using maths and f-Strings that tells us how many days, weeks, months we have left if we live until 90 years old.
+#It will take your current age as the input and output a message with our time left in this format:
+#You have x days, y weeks, and z months left.
+#Where x, y and z are replaced with the actual calculated numbers.
+#Warning your output should match the Example Output format exactly, even the positions of the commas and full stops.
+
+
+
+age = input("What is your current age?")
+
+remaining_years = 90-int(age)
+months = round(remaining_years*12)
+weeks = round(remaining_years*52)
+days = round(remaining_years*365)
+print(f"You have {days} days, {weeks} weeks, and {months} months left.")
+
