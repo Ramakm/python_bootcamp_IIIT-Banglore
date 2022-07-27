cbbb774f9e4a9db01e67d967f40b956f5e6cfcc6
Ramakm <itsramakrushna@gmail.com>
2022-07-27 12:22:02 +0000

    solution added array out of bound issue

--- caesar_cipher_encryption.py
+++ caesar_cipher_encryption.py
@@ -21,13 +21,21 @@ shift = int(input("Type the shift number:\n"))
 
 
 def encrypt(plain_text,shift_amount):
-    cipher_text = ""
+
+    cipher_text =""
+    
     for letter in plain_text:
     
         position = alphabet.index(letter)
         new_position = position +shift_amount
-        new_letter = alphabet[new_position]
-        cipher_text += new_letter
+        if new_position > 25:
+            new_position2 = new_position-26
+            new_letter2 = alphabet[new_position2]
+            cipher_text += new_letter2
+        else:
+            new_letter1 = alphabet[new_position]
+            cipher_text += new_letter1
+
     print(f"The encoded text is {cipher_text}.")
 
 encrypt(plain_text = text,shift_amount = shift)
