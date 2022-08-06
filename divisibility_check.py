812fd13fe591d25d0e07a695c94840c9522d8a95
Ramakm <itsramakrushna@gmail.com>
2022-08-06 19:27:01 +0000

    commit changes

---
+++ divisibility_check.py
@@ -0,0 +1,11 @@
+'''Description
+Given an integer ‘n’, your task is to write a Python code to find whether ‘n’ is divisible by all its digits or not. If they divide the number, then the number ‘n’ is a happy number. Otherwise, it is a sad number.
+
+The number ‘n’ may be provided with commas. At first, you have to clean the number (by removing the commas involved) and then check whether the number is happy or sad.
+---------------------------------------------------------------------------------------------------
+Input - String
+
+Output - String
+---------------------------------------------------------------------------------------------------
+Sample Input - 2,128
+Sample Output - Happy Number '''
